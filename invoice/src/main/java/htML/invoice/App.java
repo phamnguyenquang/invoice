67eacfa1f3760d061c3452bfef385c61da8461f8
quang <quang@archlinux>
2020-01-26 17:12:32 +0100

    re commit with missing file

--- invoice/src/main/java/htML/invoice/App.java
+++ invoice/src/main/java/htML/invoice/App.java
@@ -10,7 +10,7 @@ public class App {
 	public static void main(String[] args) {
 //		DataExtractor dd = new DataExtractor("/home/quang/PythonScript/invoices/test2s.html");
 //		dd.extract();
-//		new pdf2html();
-		new TrainingDataGenerator(500, "/home/quang/javaDataGen.txt");
+		new pdf2html();
+//		new TrainingDataGenerator(500, "/home/quang/javaDataGen.txt");
 	}
 }
