40bf80ae476fcafc40a8d706ac39b6ceb09dcf0b
quang <quang@archlinux>
2020-02-04 20:00:00 +0100

    xml test, failed

---
+++ invoice/src/main/java/Default/App.java
@@ -0,0 +1,19 @@
+package Default;
+
+import DataGenerator.TrainingDataGenerator;
+import NER.NERTest;
+import htML.invoice.DataExtractor;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+	public static void main(String[] args) {
+//		DataExtractor dd = new DataExtractor(5);
+//		dd.ExtractUnlabelled();
+//		new pdf2html(7);
+//		new TrainingDataGenerator(500, "resources/text/javaDataGen.txt");
+		new NERTest();
+	}
+}
