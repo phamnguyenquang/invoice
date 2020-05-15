e6f4099ca6104d22a6fbaff5dd754692bc38ca31
quang <quang@archlinux>
2020-05-15 15:15:48 +0200

    file structure fixed

--- invoice/src/main/java/Experiment/MassDataEvaluator.java
+++ invoice/src/main/java/Experiment/MassDataEvaluator.java
@@ -1,5 +1,31 @@
 package Experiment;
 
-public class MassDataEvaluator {
+import java.io.BufferedWriter;
+import java.io.File;
 
+public class MassDataEvaluator {
+	private File[] fileList;
+	private File FileDir;
+	private File[] logList;
+	private String LogDi = "/resources/coreNLP/data/Log/";
+	private boolean[] result;
+
+	private BufferedWriter[] writer;
+
+	public MassDataEvaluator(String dir) {
+		FileDir = new File(dir);
+	}
+
+	private void initiailze() {
+		int totalFile = FileDir.listFiles().length;
+		logList = new File[totalFile];
+		fileList = new File[totalFile];
+		fileList = FileDir.listFiles();
+		writer = new BufferedWriter[totalFile];
+
+	}
+
+	private void doWork() {
+
+	}
 }
